5a39baf75e6bf8f23d5dd3e1fb6d61ce24392f0e
JuanSaavedra-Tecsup <juan.saavedra.ptecsup.edu.pe>
2020-07-09 20:16:06 -0500

    commit by andy

---
+++ Snapchat/ElegirUsuarioViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ElegirUsuarioViewController.swift
+//  Snapchat
+//
+//  Created by mbtec22 on 7/9/20.
+//  Copyright © 2020 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ElegirUsuarioViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate{
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by mbtec22 on 7/9/20.
+//  Copyright © 2020 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
